567ff9ade7cf78e5394d2bb31da892a754583ca9
Punchcode <punchcode@Punchcodes-MacBook-Pro.local>
2020-05-10 22:48:00 -0700

    add first files to project

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+   $("button").addClass("animated bounce");
+});
+$(".title-1").click(function () {
+   $(".text-1").toggle();
+});
+$(".title-2").click(function () {
+   $(".text-2").toggle();
+});
+$(".title-3").click(function () {
+   $(".text-3").toggle();
+});
+$("#btnHinge").click(function () {
+   $("body").addClass("animated hinge");
+});
